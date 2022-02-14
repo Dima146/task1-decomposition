9f2bdb8210f43ad0471e99b8565ba13c03402adc
Dima146 <87914550+Dima146@users.noreply.github.com>
2022-02-14 20:03:58 +0300

    Delete Decomposition directory

--- Decomposition/Task5/NewValuesOutputting.java
+++
@@ -1,8 +0,0 @@
-package by.epam.Decomposition.Task5;
-
-public class NewValuesOutputting {
-
-    public static void outputNewValues(double num) {
-        System.out.println("New number value = " + num);
-    }
-}
